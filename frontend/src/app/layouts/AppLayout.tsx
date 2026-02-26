6c3fd40e5ce531d26ef57cf83d9ebd1ba112d4af
Sami Tugal <tugalsami@gmail.com>
2026-02-26 15:50:38 +0300

    fix: center main container in AppLayout

--- frontend/src/app/layouts/AppLayout.tsx
+++ frontend/src/app/layouts/AppLayout.tsx
@@ -5,7 +5,7 @@ export function AppLayout() {
   return (
     <div className="relative min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 container py-6">
+      <main className="flex-1 container mx-auto py-6">
         <Outlet />
       </main>
     </div>
